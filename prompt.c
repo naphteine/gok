a0e2e286f729d10dae21a4ce6719dbcb26431136
naphteine <58526116+naphteine@users.noreply.github.com>
2019-01-30 11:05:21 +0300

    Moved from fputs & fgets to readline library

--- prompt.c
+++ prompt.c
@@ -1,17 +1,21 @@
+// 2019-01-30 10:XX
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int
 main(int argc, char *argv[])
 {
-	puts("Lispy Version 0.0.0.0.1");
+	puts("Lispy Version 0.0.0.0.3");
 	puts("Press CTRL+C to exit\n");
 
 	while (1) {
-		fputs("lispy> ", stdout);
-		fgets(input, 2048, stdin);
-		printf("No you're a %s", input);
+		char *input = readline("lispy> ");
+		add_history(input);
+		printf("No you're a %s\n", input);
+		free(input);
 	}
 
 	return 0;
